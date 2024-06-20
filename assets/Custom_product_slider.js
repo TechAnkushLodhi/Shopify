7e6f87fe06cd7fadf794bbe9d4f808503451ba38
TechAnkushLodhi <ankushsinghlodhi@gmail.com>
2024-06-20 18:26:15 +0530

    Add shopify theme

---
+++ assets/Custom_product_slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  console.log("product image slider");
+  $(".Product_images_slider").slick({
+    //   // Slick options/settings go here
+    //   // infinite: true,
+    //   slidesToShow: 1,
+    //   // slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 500,
+    //   // dots: true,
+    //   // arrows: true,
+    //   // More options can be added as needed
+  });
+});
